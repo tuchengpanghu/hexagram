6d1713937fd8236c81903a1d890caa3d72371edf
panghu <tuchengpanghu@gmail.com>
2022-06-04 01:26:55 +0200

    fix typo

--- hexa.js
+++ hexa.js
@@ -30,7 +30,8 @@ var draw = SVG(document.documentElement)
 // TODO: center canvas in the screen
 draw.size(total_width, total_height)
 
-function bar(num, text){
+function bar(num, text_arr){
+    len = text_arr.length
     var rect = draw.rect(base_width * 64, base_height)
                     .attr({'stroke-width': 1, 'stroke': "black", 'fill': "black"})
 
@@ -39,21 +40,29 @@ function bar(num, text){
                     .attr({'stroke-dasharray': base_width*64/num})
                     .move(0, base_height/2)
 
-    var text = draw.plain(text).font('size', 0).move(base_width*32,base_height/2).attr(_text_attr)
-
     var group = draw.group()
-    group.add(rect).add(line).add(text)
+    group.add(rect).add(line)
+
+    for (let i = len; i > 0; i--) {
+        block = base_width*64/len
+        position = block*i - block/2
+        var text = draw.plain(text_arr[len-i]).font('size', 0).move(position, base_height/2).attr(_text_attr)
+        if ((len-i) % 2 == 1){
+            text.fill("white")
+        }
+        group.add(text)
+    }
 
     return group
 }
 
-bar(64).move(padding,(base_height+2)*2)
-bar(32).move(padding,(base_height+2)*3)
-bar(16).move(padding,(base_height+2)*4)
-bar(8).move(padding,(base_height+2)*5)
-bar(4).move(padding,(base_height+2)*6)
-bar(2).move(padding,(base_height+2)*7)
-bar(1, '太 極').move(padding,(base_height+2)*8)
+bar(64, []).move(padding,(base_height+2)*2)
+bar(32, []).move(padding,(base_height+2)*3)
+bar(16, []).move(padding,(base_height+2)*4)
+bar(8, ["乾","兌","離","震","巽","坎","艮","坤"]).move(padding,(base_height+2)*5)
+bar(4, ["太陽","少陰","少陽","太陰"]).move(padding,(base_height+2)*6)
+bar(2, ["陽","陰"]).move(padding,(base_height+2)*7)
+bar(1, ["太 極"]).move(padding,(base_height+2)*8)
 
 
 console.log(draw.svg())
