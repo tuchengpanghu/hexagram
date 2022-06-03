ecd8fa79f051665a2e8ceb59bc7541ff1b7291a6
panghu <tuchengpanghu@gmail.com>
2022-06-03 23:26:48 +0200

    fix typo

--- hexa.js
+++ hexa.js
@@ -12,28 +12,34 @@ var base_height = 50
 var base_width = 22
 var padding = base_width
 var total_width = base_width * 64 + padding * 3
-var total_height = base_height * 8
+var total_height = base_height * 10
 
 // create canvas
 var draw = SVG(document.documentElement)
 // draw pink square
 draw.size(total_width, total_height)
 
-function bar(step){
-    var line = draw.line(0, 0, base_width*64, 0)
-    .attr({'stroke-width': base_height, 'stroke': "black"})
-    .attr({'stroke-dasharray': base_width*step})
+function bar(num){
+    var line = draw.line(base_width*64, 0, 0, 0)
+    .attr({'stroke-width': base_height, 'stroke': "white"})
+    .attr({'stroke-dasharray': base_width*64/num})
     .move(0, base_height/2)
 
     var rect = draw.rect(base_width * 64, base_height)
-                    .attr({'stroke-width': 1, 'stroke': "black", 'fill': "none"})
+                    .attr({'stroke-width': 1, 'stroke': "black", 'fill': "black"})
 
     var group = draw.group()
-    group.add(line).add(rect)
+    group.add(rect).add(line)
 
     return group
 }
 
-bar(1).move(padding,10)
+bar(64).move(padding,(base_height+2)*2)
+bar(32).move(padding,(base_height+2)*3)
+bar(16).move(padding,(base_height+2)*4)
+bar(8).move(padding,(base_height+2)*5)
+bar(4).move(padding,(base_height+2)*6)
+bar(2).move(padding,(base_height+2)*7)
+bar(1).move(padding,(base_height+2)*8)
 
 console.log(draw.svg())
