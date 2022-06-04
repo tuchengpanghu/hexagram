9182f2e3c27c85feddef1353cf091ecd5cbe8111
panghu <tuchengpanghu@gmail.com>
2022-06-04 11:58:35 +0200

    fix typo

--- hexa.js
+++ hexa.js
@@ -15,6 +15,16 @@ var padding = base_width
 var total_width = base_width * 64 + padding * 3
 var total_height = base_height * 10
 
+var _title_attr = {
+    'font-family': "Hiragino Sans CNS",
+    'font-size': 45,
+    'font-weight': 'bold',
+    'text-anchor': 'middle',
+    'dominant-baseline': 'central',
+    'direction': 'rtl',
+    'unicode-bidi': 'bidi-override'
+}
+
 var _text_attr = {
     'font-family': "Hiragino Sans CNS",
     'font-size': 35,
@@ -25,13 +35,23 @@ var _text_attr = {
     'unicode-bidi': 'bidi-override'
 }
 
+var _other_attr = {
+    'font-family': "Hiragino Sans GB",
+    'font-size': 15,
+    'writing-mode': 'vertical-lr',
+    'text-anchor': 'middle',
+    // text-orientation: mixe,
+}
+
 var _title = "伏羲六十四卦次序"
 var _data = {
     "太極": ["太 極"],
     "兩儀": ["陽", "陰"],
     "四象": ["太陽", "少陰", "少陽", "太陰"],
     "八卦": ["乾", "兌", "離", "震", "巽", "坎", "艮", "坤"],
-    "六十四卦": ['乾', '夬', '大有', '大壯', '小畜', '需', '大畜', '泰',
+    "十六": [],
+    "三十二": [],
+    "六十四": ['乾', '夬', '大有', '大壯', '小畜', '需', '大畜', '泰',
                 '履', '兌', '睽', '歸妹', '中孚', '節', '損', '臨',
                 '同人', '革', '離', '豐', '家人', '既濟', '賁', '明夷',
                 '無妄', '隨', '噬嗑', '震', '益', '屯', '頤', '複',
@@ -48,7 +68,20 @@ var draw = SVG(document.documentElement)
 // TODO: center canvas in the screen
 draw.size(total_width, total_height)
 
-function bar(num, text_arr){
+function title(t){
+    var position = base_width*32
+    var rect = draw.rect(base_width * 64, base_height)
+                   .attr({'stroke-width': 1, 'stroke': "none", 'fill': "none"})
+    var t = draw.plain(t.split('').join(' ')).font('size', 0).move(position, base_height/2).attr(_title_attr)
+
+    var group = draw.group()
+    group.add(rect).add(t)
+
+    return group
+}
+
+function bar(num, item){
+    text_arr = _data[item]
     len = text_arr.length
     var rect = draw.rect(base_width * 64, base_height)
                     .attr({'stroke-width': 1, 'stroke': "black", 'fill': "black"})
@@ -65,11 +98,16 @@ function bar(num, text_arr){
         // writing from right to left
         block = base_width*64/len
         position = block*i - block/2
-        // set initial size to 0, to avoid move scale
+        // set initial size to 0, to avoid moving drift
         var text = draw.plain(text_arr[len-i]).font('size', 0).move(position, base_height/2).attr(_text_attr)
         if ((len-i) % 2 == 1){
             text.fill("white")
         }
+        group.add(text)
+    }
+
+    if (item != "太極"){
+        var text = draw.plain(item).font('size', 0).move(base_width*65, base_height/2).attr(_other_attr)
         group.add(text)
     }
 
@@ -78,13 +116,14 @@ function bar(num, text_arr){
 
 var gap = 4
 
-bar(64, []).move(padding,(base_height+gap)*2)
-bar(32, []).move(padding,(base_height+gap)*3)
-bar(16, []).move(padding,(base_height+gap)*4)
-bar(8, ["乾","兌","離","震","巽","坎","艮","坤"]).move(padding,(base_height+gap)*5)
-bar(4, ["太陽","少陰","少陽","太陰"]).move(padding,(base_height+gap)*6)
-bar(2, ["陽","陰"]).move(padding,(base_height+gap)*7)
-bar(1, ["太 極"]).move(padding,(base_height+gap)*8)
+title(_title).move(padding,gap*2)
 
+bar(64, "六十四").move(padding,(base_height+gap)*2)
+bar(32, "三十二").move(padding,(base_height+gap)*3)
+bar(16, "十六").move(padding,(base_height+gap)*4)
+bar(8, "八卦").move(padding,(base_height+gap)*5)
+bar(4, "四象").move(padding,(base_height+gap)*6)
+bar(2, "兩儀").move(padding,(base_height+gap)*7)
+bar(1, "太極").move(padding,(base_height+gap)*8)
 
 console.log(draw.svg())
